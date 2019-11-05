ca5d451d4cfaacd4eed07894d047a093b6f010d2
yoopersteeze <31364490+yoopersteeze@users.noreply.github.com>
2019-11-04 20:18:40 -0600

    Update uninstall-mysql.sh

--- uninstall-mysql.sh
+++ uninstall-mysql.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 echo "This script will now remove all MySQL files from the system."
 rm -rf /usr/local/mysql*
+rm -rf /usr/local/var/mysql
 rm -rf /Library/StartupItems/MySQLCOM
 rm -rf /Library/PreferencePanes/My*
 rm -rf /Library/Receipts/mysql*
 rm -rf /var/db/receipts/com.mysql.*
 rm -rf /var/mysql
-echo "Moving my.cnf to Desktop"
-mv /etc/my.cnf ~/Desktop/
+echo "Removing previous mysql preferences"
+rm -rf /Library/Receipts/mysql*
+rm -rf /Library/Receipts/MySQL*
+rm -rf /private/var/db/receipts/*mysql*
 rm -rf /usr/bin/mysql*
+echo "Moving my.cnf to Desktop"
+mv /etc/my.cnf ~/Desktop/my-old.cnf.bak
 echo "This script has finished removing MySQL from the system, and will now remove it from any startup or launch processes."
 rm -rf /Library/LaunchDaemons/*mysql*
 rm -rf /Library/LaunchAgents/*mysql*
